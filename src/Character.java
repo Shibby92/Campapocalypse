540647d161b2d2875ad92de8da88435267564a34
Shibby92 <haris_000@hotmail.com>
2014-12-18 10:10:38 +0100

    CampApocalypse
    Class character

---
+++ src/Character.java
@@ -0,0 +1,74 @@
+public class Character {
+	private String name;
+	private double hp;
+	private double dexterity;
+	private Weapon weapon;
+	private double strength;
+	private double speed;
+
+	public attack() {
+
+	}
+
+	public specialAttack() {
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getHp() {
+		return hp;
+	}
+
+	public void setHp(double hp) {
+		this.hp = hp;
+	}
+
+	public double getDexterity() {
+		return dexterity;
+	}
+
+	public void setDexterity(double dexterity) {
+		this.dexterity = dexterity;
+	}
+
+	public Weapon getWeapon() {
+		return weapon;
+	}
+
+	public void setWeapon(Weapon weapon) {
+		this.weapon = weapon;
+	}
+
+	public double getStrength() {
+		return strength;
+	}
+
+	public void setStrength(double strength) {
+		this.strength = strength;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Name: " + getName() + "\nHP: " + getHp() + "\nDexterity: "
+				+ getDexterity() + "\nWeapon: " + this.weapon.toString()
+				+ "\nStrength: " + getStrength() + "\nSpeed: " + getSpeed());
+
+		return sb.toString();
+	}
+
+}
