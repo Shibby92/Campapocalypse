6de23d8d15b45c8286e9c9dd49c4d263463e13df
Shibby92 <haris_000@hotmail.com>
2014-12-18 18:57:23 +0100

    v2.0
    Potpuna igra!

--- src/Player.java
+++ src/Player.java
@@ -1,59 +1,48 @@
-
 public class Player extends Character {
 
-	
 	private double inteligence;
-	
-	
-	public Player(String name, double hp, double dex, Weapon weapon, double strenght, double speed, double inteligence){
-		super(name, hp, dex, weapon, strenght, speed );
-		this.inteligence=inteligence;
+
+	public Player(String name, double hp, double dex, Weapon weapon,
+			double strenght, double speed, double inteligence) {
+		super(name, hp, dex, weapon, strenght, speed);
+		this.inteligence = inteligence;
 	}
-	
-	
-	
-	
-	
+
 	public double getInteligence() {
-		this.inteligence=0.5+Math.random()*(2-(0.5+1));
+		this.inteligence = 0.5 + Math.random() * (2 - 0.5) + 1;
 		return inteligence;
 	}
 
-
-	public double  attack(){
+	public double attack() {
 		double vrijednostAtt;
-		vrijednostAtt=super.attack()*getInteligence();
+		vrijednostAtt = super.attack() * getInteligence();
 		return vrijednostAtt;
 	}
-	
-	
 
-	public String toString (){
+	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("Name: " + getName() + "\nHP: " + getHp() + "\nDexterity: "
 				+ getDexterity() + "\nWeapon: " + getWeapon().toString()
-				+ "\nStrength: " + getStrength() + "\nSpeed: " + getSpeed()+"\nInteligence : " + inteligence);
+				+ "\nStrength: " + getStrength() + "\nSpeed: " + getSpeed()
+				+ "\nInteligence : " + inteligence);
 
 		return sb.toString();
-		
+
 	}
-	
-	
-	public boolean escChance(Character z){
-		 int chance = (int) ((this.inteligence*this.getSpeed())/ z.getSpeed());
+
+	public boolean escChance(Character z) {
+		int chance = (int) ((this.inteligence * this.getSpeed()) / z.getSpeed());
 		return chance >= 1 ? true : false;
 	}
-	public boolean surviveBattle(Character other){
-		while(true){
-			other.setHp(other.getHp()-this.attack());
-			this.setHp(this.getHp()-other.attack());
-			if (other.getHp()<0 || this.getHp()<0 ) {
-				break;
-			}
+
+	public boolean surviveBattle(Character other) {
+		while (other.getHp() > 0 && this.getHp() > 0) {
+			other.setHp(other.getHp() - this.attack());
+			this.setHp(this.getHp() - other.attack());
 		}
-		if(this.getHp()>0)
+		if (this.getHp() > 0)
 			return true;
-			return false;
-		
+		return false;
+
 	}
 }
--- src/TestGame.java
+++ src/TestGame.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class TestGame {
 
 	public static void main(String[] args) {
-		System.out.println("Bila je strasna i tmurna noc...sinoc...danas  je vec druga situacija suncano, cak \nmjestimicno oblacno naravno"
-				+ "trebamo bniti iskreni i reci da je onako cak pretoplo ya \n ovo doba godine....probas da se sakrije od ove odvratne  zege u jednoj staroj napustenoj "
-				+ "kuci. Cujes nesto tamo suska, realno boli trebe briga sta \nsuska ti probas da nadjes lampu...a ono suskaaaaaa.....nadjes lampu ... \npalis je...."
-				+ "KAD IMAS STA VIDJET EMINA....");
-		
-		Player first = new Player("Haris", 150, 0.5, null, 8, 19, 2);
+		System.out
+				.println("Bila je strasna i tmurna noc...sinoc...danas  je vec druga situacija suncano, cak \nmjestimicno oblacno naravno"
+						+ "trebamo bniti iskreni i reci da je onako cak pretoplo ya \n ovo doba godine....probas da se sakrije od ove odvratne  zege u jednoj staroj napustenoj "
+						+ "kuci. Cujes nesto tamo suska, realno boli trebe briga sta \nsuska ti probas da nadjes lampu...a ono suskaaaaaa.....nadjes lampu ... \npalis je...."
+						+ "KAD IMAS STA VIDJET EMINA....");
+
+		Player first = new Player("Haris", 350, 0.5, null, 8, 5, 2);
 		// System.out.println(first.attack());
 		// Emina mina=new Emina ("Emina",97,0.7,null,8,15);
 		// Smoker smoke=new Smoker ("SmEmina",80,0.6,null,7,14);
@@ -20,7 +21,7 @@ public class TestGame {
 		Zombie[] prvi = new Zombie[15];
 		while (br < 15) {
 
-			int broj = (int) (1 + (Math.random() * (5 )));
+			int broj = (int) (1 + (Math.random() * (5)));
 			switch (broj) {
 			case 1:
 				prvi[br] = new Emina("Emina", 97, 0.7, null, 8, 15);
@@ -41,12 +42,40 @@ public class TestGame {
 			}
 			br++;
 		}
-		if (first.surviveBattle(prvi[0])){
-			System.out.println("Prezivjeli ste!");
-		System.out.println("Ostalo vam je " + first.getHp() + " HP!");
+		int run;
+		for (int i = 0; i < prvi.length; i++) {
+			System.out.println("Novi ZOMBIE... Bjezati ili ne?");
+			run = in.nextInt();
+			if (run == 1 && !first.escChance(prvi[i])) {
+				System.out.println("Niste uspjeli pobjeci, bijte se!\n.\n.BUM!\n.\n.TRS!\n.\n.POAW!\n.");
+				if (first.surviveBattle(prvi[i])) {
+					System.out.println("Prezivjeli ste " + (i + 1)
+							+ ". zombie!");
+					System.out.println("Ostalo vam je " + first.getHp()
+							+ " HP!");
+				} else {
+					System.out.println("JEBIGA SREO SI EMINU...");
+					break;
+
+				}
+
+			} else if(first.escChance(prvi[i])){
+				System.out.println("Uspjesno ste pobjegli!");
+			}
+			else if(run==0){
+				if (first.surviveBattle(prvi[i])) {
+					System.out.println("Prezivjeli ste " + (i + 1)
+							+ ". zombie!");
+					System.out.println("Ostalo vam je " + first.getHp()
+							+ " HP!");
+				} else {
+					System.out.println("JEBIGA SREO SI EMINU...");
+					break;
+				}
+				}
+		
+			
 		}
-		else 
-			System.out.println("JEBIGA SREO SI EMINU...");
 	}
 
 }
