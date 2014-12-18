cfc2b84544111464e26a248f07df6e9ea4a2917b
Shibby92 <haris_000@hotmail.com>
2014-12-18 12:33:35 +0100

    v1.05
    escapeChance

--- src/Player.java
+++ src/Player.java
@@ -1,43 +1,39 @@
-
 public class Player extends Character {
 
-	
 	private double inteligence;
-	
-	
-	public Player(String name, double hp, double dex, Weapon weapon, double strenght, double speed, double inteligence){
-		super(name, hp, dex, weapon, strenght, speed );
-		this.inteligence=inteligence;
+
+	public Player(String name, double hp, double dex, Weapon weapon,
+			double strenght, double speed, double inteligence) {
+		super(name, hp, dex, weapon, strenght, speed);
+		this.inteligence = inteligence;
 	}
-	
-	
-	
+
 	public double getInteligence() {
 		return inteligence;
 	}
 
-
-
-	public String toString (){
+	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("Name: " + getName() + "\nHP: " + getHp() + "\nDexterity: "
 				+ getDexterity() + "\nWeapon: " + getWeapon().toString()
-				+ "\nStrength: " + getStrength() + "\nSpeed: " + getSpeed()+"\nInteligence : " + inteligence);
+				+ "\nStrength: " + getStrength() + "\nSpeed: " + getSpeed()
+				+ "\nInteligence : " + inteligence);
 
 		return sb.toString();
-		
+
 	}
-	
-	public double attack(){
+
+	public double attack() {
 		double initAttack = super.attack();
 		System.out.println(initAttack);
 		initAttack *= inteligence;
 		return initAttack;
 	}
-	
-	public double escChance(){
-		return 0;
+
+	public boolean escChance(Zombie other) {
+		if ((int) ((this.inteligence * this.getSpeed()) / other.getSpeed()) != 0)
+			return true;
+		return false;
 	}
 
-	
 }
