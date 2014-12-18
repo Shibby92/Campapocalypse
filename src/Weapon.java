661f85d4abb0df67fb92cd3b0a2e784e093237bd
GorjanGOA <gorjankalauzovic@BitCamps-Mac-mini-6.local>
2014-12-18 10:30:48 +0100

    Amra i Gorjan prvi commit

---
+++ src/Weapon.java
@@ -0,0 +1,15 @@
+
+public class Weapon {
+
+	private double dmg;
+	private double critical;
+	private double ammo;
+	
+	public Weapon(){
+		this.dmg=0;
+		this.critical=0;
+		this.ammo=0;
+	}
+	
+	
+}
